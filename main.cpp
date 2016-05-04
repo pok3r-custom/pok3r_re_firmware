ed446dcc320644908f0950ca1e101f2cfd020fcb
Charlie Waters <cawiii@me.com>
2016-05-04 09:43:02 -0400

    add commands

--- main.cpp
+++ main.cpp
@@ -1,25 +1,44 @@
 #include "zlog.h"
 #include "pok3r.h"
 
-int main(){
-    ZLog::logLevelStdOut(ZLog::INFO, "%time% %thread% N %log%");
-    ZLog::logLevelStdErr(ZLog::ERROR, "\x1b[31m%time% %thread% E [%file%:%line%] %log%\x1b[m");
-
+int readver(){
     LOG("Looking for Vortex Pok3r...");
-
     Pok3r pok3r;
     if(pok3r.findPok3r()){
         if(pok3r.open()){
             LOG("Found: " << pok3r.getVersion());
-
-            LOG("Done");
+            return 0;
         } else {
             LOG("Failed to Open");
+            return -2;
         }
     } else {
         LOG("Not Found");
+        return -1;
     }
+}
+
+int decfw(ZPath exe){
 
     return 0;
 }
 
+int main(int argc, char **argv){
+    ZLog::logLevelStdOut(ZLog::INFO, "%time% %thread% N %log%");
+    ZLog::logLevelStdErr(ZLog::ERROR, "\x1b[31m%time% %thread% E [%file%:%line%] %log%\x1b[m");
+
+    if(argc > 1){
+        ZString cmd = argv[1];
+        if(cmd == "readver"){
+            return readver();
+        } else if(cmd == "decfw"){
+            return decfw();
+        } else {
+            LOG("Unknown Command \"" << cmd << "\"");
+            return 1;
+        }
+    } else {
+        LOG("No Command");
+        return 1;
+    }
+}
