b4569e93a8425aa338aceeffa3f5c615153b8b6e
Charlie Waters <cawiii@me.com>
2019-10-23 19:58:01 -0400

    flash script

--- util/flash.py
+++ util/flash.py
@@ -1,11 +1,24 @@
 #!python3
 
 import sys, os, time
+import shutil
 import pylink
 
 #libraries = list(pylink.Library.find_library_linux())
 #for lib in libraries:
 #    print("  " + lib)
+
+xml = os.path.join(os.path.dirname(__file__), "jlink_flashloader/JLinkDevices.xml")
+devdir = os.path.join(os.path.dirname(__file__), "jlink_flashloader/Holtek")
+shutil.copyfile(xml, "/tmp/JLinkDevices.xml")
+try:
+    os.mkdir("/tmp/Devices")
+except:
+    pass
+try:
+    shutil.copytree(devdir, "/tmp/Devices/Holtek")
+except:
+    pass
 
 jlink = pylink.JLink()
 #jlink = pylink.JLink(log=open("log.txt", "w"))
@@ -13,10 +26,14 @@ jlink = pylink.JLink()
 print("Library: " + jlink._library._path)
 #print("Library: " + jlink._library._temp.name)
 
+# devname = "HT32F1655"
+devname = "HT32F52352"
+
 print("Devices: " + str(jlink.num_supported_devices()))
 for i in range(jlink.num_supported_devices()):
     device = jlink.supported_device(i)
-    if device.name == "HT32F1655":
+    # print(device.name)
+    if device.name.startswith(devname):
         print("Found: " + device.name)
 
 print("Open JLink")
@@ -32,9 +49,12 @@ print("Firmware: " + jlink.firmware_version)
 #check = 0xFFFFFFE0 + 0xFFFFFFFF + 0xFFFFFFFF + 0xFFFFFFFF + 0xFFFFFFFC
 check = 0xFFFFFFE0 + 0xFFFFFFFF + 0xFFFFFFFF + 0xFFFFFFFF + 0xFFFFFFFF
 check = check & 0xFFFFFFFF
-print("%X" % check)
+print("Checksum: %X" % check)
 
 jlink.set_tif(pylink.enums.JLinkInterfaces.SWD)
-jlink.connect("HT32F1655", 4000, verbose=True)
+jlink.connect(devname, 4000, verbose=True)
 
 print(jlink.register_list())
+
+btable = jlink.memory_read(0x0, 32)
+print("%s" % " ".join([ "%02X" % x for x in btable ]))
